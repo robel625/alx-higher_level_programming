e0462ccf51dbca53f43f8b4bafdb36245a1c55e0
Robel Gulima <robelgulima@gmail.com>
2022-02-28 13:26:21 -0800

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const len = process.argv.length;
-if (len < 4) {
-  console.log(0);
-} else {
-  const numArray = process.argv.sort((a, b) => a - b);
-  console.log(numArray[numArray.length - 2]);
-}
--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-exports.add = function (a, b) {
-  const c = a + b;
-  return c;
-};
