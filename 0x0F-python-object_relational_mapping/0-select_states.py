32339e82bef059ecc05b31138814ea0e58663d69
robel625 <76595851+robel625@users.noreply.github.com>
2022-03-06 00:29:10 -0800

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-# script that lists all states from the database hbtn_0e_0_usa
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
 
-
-def getStates(userName, passWord, dbName):
-    """ Accesses database hbtn_0e_0_usa and grabs states, then puts in
-    ascending order.
-    ARGS:
-        userName: the username
-        passWord: the password
-        dbName: the name of the database to access
-    """
-
-    import MySQLdb
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=userName,
-        passwd=passWord,
-        db=dbName,
-        charset="utf8"
-    )
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    """ Take in arguments and passes to get states from db """
-    from sys import argv
-
-    getStates(argv[1], argv[2], argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
