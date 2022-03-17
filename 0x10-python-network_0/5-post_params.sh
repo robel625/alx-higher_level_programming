d1c754f27a4af3237994155a1c488f12b2cb4df3
robel625 <76595851+robel625@users.noreply.github.com>
2022-03-17 14:02:07 -0700

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends POST request and displays body response
-curl -sX "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX "POST" "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
